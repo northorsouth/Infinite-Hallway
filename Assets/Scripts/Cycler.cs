e0909c4227b2ee4049823dd729713cd5d97ae213
Dakota Crouchelli <earthquakedriver@protonmail.com>
2019-03-08 23:06:10 -0500

    Added Infinite Hallway

---
+++ Assets/Scripts/Cycler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cycler : MonoBehaviour
+{
+    public float CellSize = 5;
+    public Transform target;
+
+    private Transform first;
+    private Transform last;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.childCount <= 1)
+            Debug.Log("What the fuck dude");
+        
+        first = transform.GetChild(0);
+        last = transform.GetChild(transform.childCount-1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (((last.position - target.position).magnitude - (first.position - target.position).magnitude) >= CellSize)
+        {
+            last.SetAsFirstSibling();
+            last.position = first.position + ((first.position - last.position).normalized * CellSize);
+
+            first = last;
+            last = transform.GetChild(transform.childCount-1);
+            Debug.Log("Closer to first");
+        }
+
+        if (((first.position - target.position).magnitude - (last.position - target.position).magnitude) >= CellSize)
+        {
+            first.SetAsLastSibling();
+            first.position = last.position + ((last.position - first.position).normalized * CellSize);
+
+            last = first;
+            first = transform.GetChild(0);
+            Debug.Log("Closer to last");
+        }
+    }
+}
---
+++ Assets/Scripts/Looper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Looper : MonoBehaviour
+{
+    public enum loopAxis { x, y, z };
+    public loopAxis LoopAxis = loopAxis.z;
+    
+    public float maxDistance = 25;
+
+    private float startPos;
+
+    private CharacterController character;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        startPos = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.z) - startPos >= maxDistance)
+        {
+            Vector3 tempPos = transform.position;
+            tempPos.z = startPos;
+            character.enabled = false;
+            transform.position = tempPos;
+            character.enabled = true;
+        }
+    }
+}
